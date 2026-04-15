35d30e67b5cbe56cf4268b19507b42a45b457c00
0electricista <karimelhaidini@hotmail.com>
2026-04-15 13:16:41 +0200

    Plantilla VideoMiner

--- VideoMiner/src/main/java/aiss/videominer/VideominerApplication.java
+++ VideoMiner/src/main/java/aiss/videominer/VideominerApplication.java
@@ -1,13 +1,13 @@
-package com.aiss.videominer;
+package aiss.videominer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideoMinerApplication {
+public class VideominerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideoMinerApplication.class, args);
+		SpringApplication.run(VideominerApplication.class, args);
 	}
 
 }
