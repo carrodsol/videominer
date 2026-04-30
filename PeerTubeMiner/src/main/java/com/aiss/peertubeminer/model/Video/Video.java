e23af77c0ee90452b8f10dd07d3fb88020b7aad7
CARLOS RODRIGUEZ <carrodsol@alum.us.es>
2026-04-30 13:21:02 +0200

    New POJO classes added

--- PeerTubeMiner/src/main/java/com/aiss/peertubeminer/model/Video/Video.java
+++ PeerTubeMiner/src/main/java/com/aiss/peertubeminer/model/Video/Video.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-// TODO
+// TODO: limpieza de POJO
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Video {
 
