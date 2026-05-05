dd8b3e84e90fc80e3e4ad19123f543784236e9f2
CARLOS RODRIGUEZ <carrodsol@alum.us.es>
2026-05-05 20:37:57 +0200

    UserETL.java

--- PeerTubeMiner/src/main/java/com/aiss/peertubeminer/etl/UserETL.java
+++ PeerTubeMiner/src/main/java/com/aiss/peertubeminer/etl/UserETL.java
@@ -12,25 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserETL {
 
-    private final UserService userService;
-
-    public UserETL(UserService userService) {
-        this.userService = userService;
-    }
     public VMUser transform(PTAccount account) {
-        if (account == null) {
-            return null;
+        VMUser vmUser = new VMUser();
+        vmUser.setName(account.getName());
+        vmUser.setUser_link(account.getUserLink());
+
+        if (account.getAvatars() != null && !account.getAvatars().isEmpty()) {
+            vmUser.setPicture_link(account.getAvatars().get(0).getPictureLink());
         }
 
-        VMUser user = new VMUser();
-        user.setName(account.getName());
-        user.setUser_link(account.getUserLink());
-
-        List<PTAccountAvatar> avatars = account.getAvatars();
-        if (avatars != null && !avatars.isEmpty() && avatars.get(0) != null) {
-            user.setPicture_link(avatars.get(0).getPictureLink());
-        }
-
-        return user;
+        return vmUser;
     }
 }
