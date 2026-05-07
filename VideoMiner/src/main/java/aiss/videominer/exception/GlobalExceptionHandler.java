2519e1092a1d30580f1d4b046aa427a948491a39
Cinatik <juliancara05@gmail.com>
2026-05-07 13:30:09 +0200

    Excepciones más específicas para las 3 apis

--- VideoMiner/src/main/java/aiss/videominer/exception/GlobalExceptionHandler.java
+++ VideoMiner/src/main/java/aiss/videominer/exception/GlobalExceptionHandler.java
@@ -1,5 +1,48 @@
 package aiss.videominer.exception;
 
-public class GlobalExceptionHandler extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GlobalExceptionHandler extends Exception {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseEntity<Map<String, List<String>>> handleValidationException(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        List<String> errors = fieldErrors.stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+        Map<String, List<String>> res = new HashMap<>();
+        res.put("errors", errors);
+        return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+    }
+
+    // Error 400: Se activa por parámetros incorrectos (como maxVideos o maxPages mal escritos)
+    @ExceptionHandler({HttpClientErrorException.BadRequest.class, MethodArgumentTypeMismatchException.class})
+    @ResponseBody
+    public ResponseEntity<Object> handleBadRequest(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Parámetros incorrecto de búsqueda");
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    // Errores 5xx: Error genérico del servidor
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<Object> handleGeneralException(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Error interno del servidor");
+        ex.printStackTrace();
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
